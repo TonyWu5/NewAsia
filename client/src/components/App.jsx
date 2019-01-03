eb369adce45031da4d36bb8555064601cfda203c
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2019-01-02 20:17:46 -0800

    Install babel, webpack moduels and creat webpack.config file

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello From React</p>
+      </div>
+    )
+  }
+}
+
+export default App;
