532925709fd68187600e3d98e294983cc8f8c64e
Simon <simonyuanliu@gmail.com>
2019-01-08 12:01:05 -0800

    added grid to events

---
+++ client/src/Events.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Events extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="mainPicture col-8">
+              Main Picture and model
+          </div>
+          <div className="sidePicture col-4">
+            <div>sidePicture  contact Info</div>
+            <div>sidePicture and links</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
