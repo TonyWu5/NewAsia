3cf89738d1e4dcc32b92d10ff9e2b7dc60ed5e5f
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2019-01-11 18:06:27 -0800

    Add basic react-route setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from './Home.jsx';
+import History from './History.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>Hello From React</p>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/history">History</Link>
+            </li>
+          </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/history" component={History} />
+        </div>
+      </Router>
     )
   }
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a basic Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
