8b13dc20e48017461887a442b2bcfc91f63f671b
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2019-01-14 10:32:10 -0800

    Add event link

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -3,7 +3,6 @@ import { NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
 
 class NavLink extends React.Component{
-  
   render(){
     return(
         <NavItem onClick={() => this.props.onClick()} eventKey={this.props.key} href="#" active={this.props.isActive}>
