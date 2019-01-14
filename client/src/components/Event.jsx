f1e8e1226ec1b11c6bf569bf7c22987d3e5e156b
Simon <simonyuanliu@gmail.com>
2019-01-14 10:18:11 -0800

    Add eventPage

---
+++ client/src/components/Event.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Events extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="mainPicture ">
+              Main Picture and model
+          </div>
+          <div className="sidePicture ">
+            <div>sidePicture  contact Info</div>
+            <div>sidePicture and links</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
